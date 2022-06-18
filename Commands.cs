9235650c5235c5d15a2ad09f71d6039f0596ace9
Nigmatulin Timur <timurnigmatulin6@gmail.com>
2022-06-18 13:31:22 +0500

    bot_1.1
    рефакторинг

---
+++ Commands.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Audio;
+using Sodium;
+using Xabe.FFmpeg;
+using Concentus;
+
+namespace MusicallityBot
+{
+    public  class Commands
+    {
+        private IVoiceChannel channel;
+
+        public Commands(SocketMessage msg)
+        {
+            channel = ((IGuildUser)msg.Author).VoiceChannel;
+        }
+        public async  void PlayMusicInVoiceChat(SocketMessage msg)
+        {
+            channel = channel ?? ((IGuildUser)msg.Author).VoiceChannel;
+            if (channel == null) { await msg.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }         
+            IAudioClient audioClient = null;
+            audioClient = await channel.ConnectAsync(true);
+
+        }
+        public  async void DeleteBotFromVoiceChat()
+        {
+            await channel.DisconnectAsync();
+            channel = null;
+        }
+    }
+}
